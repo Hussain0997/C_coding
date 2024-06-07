78045f2447d3a8e134dea8bc9d80fa16de26c5c5
Hussain0997 <hussainali0260444@gmail.com>
2024-06-07 04:05:26 -0700

    Add files via upload

---
+++ Bassic/simple cal.c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code her
+    int num1 = 4 , num2 = 6;
+    int add;
+    int sub;
+    int mul;
+    add = num1 + num2;
+    sub = num1 - num2;
+    mul = num1 * num2;
+    printf("add = %d ,sub = %d ,mul = %d", add,sub,mul);
+
+    return 0;
+}
---
+++ Bassic/user input.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    printf("enter num1 ");
+    scanf("%d",&num1);
+    printf("enter num2 ");
+    scanf("%d", &num2);
+    
+    printf("sum = %d ",num1+num2);
+    printf("sum = %d ",num1-num2);
+    printf("sum = %d ",num1*num2);
+    
+    
+
+    return 0;
+}
